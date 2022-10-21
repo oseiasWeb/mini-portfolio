24366d1d587fe2bdf96329c1c67e8c571526808b
Oseias Nogueira <oseias.n.m@gmail.com>
2022-10-21 01:51:46 -0300

    Ajustes finais de elementos

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,2 +1,27 @@
 // Pegar os elementos que representam as abas no HTML
 const abas = document.querySelectorAll(".aba");
+
+// Identificar o click no elemento da aba
+abas.forEach(aba => {
+    aba.addEventListener("click", function(){
+
+        if(aba.classList.contains("selecionado")){
+            return;
+        }
+
+        const abaSelecionada = document.querySelector(".aba.selecionado");
+        abaSelecionada.classList.remove("selecionado")
+
+        aba.classList.add("selecionado");
+
+        const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+        informacaoSelecionada.classList.remove("selecionado");
+
+        // Mostrar conteúdo da aba selecionada
+        const idElementoDeInformacoesAba = `informacao-${aba.id}`
+
+        const informacaoAserMostratada = document.getElementById(idElementoDeInformacoesAba)
+        informacaoAserMostratada.classList.add("selecionado")
+
+    });
+});
